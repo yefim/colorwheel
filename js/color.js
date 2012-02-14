f9cc39f8abbc69e14e775ec64ee59906ec144ba8
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-02-14 02:41:10 -0500

    pulled certain functions into the color class

--- js/color.js
+++ js/color.js
@@ -3,7 +3,35 @@ function Color(red,green,blue) {
   this.g = green;
   this.b = blue;
 }
-Color.prototype.toString() {
-  return 'rgb(' + r + "," + g "," + "c" + ')';
+Color.prototype.toString = function() {
+  return 'rgb('+r+","+g","+b +')';
 }
-
+Color.prototype.toRGB = function(num) {
+  return parseInt(num, 16);
+}
+Color.prototype.toHex = function(num) {
+  return parseInt(num,10).toString(16);
+}
+Color.prototype.parseHex = function(hex) {
+  if (hex.charAt(0) === '#') hex = hex.substr(1);
+  var hex_regex = /^(?:[0-9a-fA-F]{3}){1,2}$/;
+  if (hex.match(hex_regex)) {
+    var c = [];
+    if (hex.length === 6) {
+      c = hex.match(/.{1,2}/g);
+    } else if (hex.length === 3) {
+      c = [hex[0]+hex[0], hex[1]+hex[1], hex[2]+hex[2]];
+    }
+    for (var i = 0; i < c.length; i++) {
+      c[i] = toRGB(c[i]);
+    }
+    return 'rgb(' + c.join(',') + ')';
+  }
+}
+Color.prototype.parseRGB = function(rgb) {
+  var rgb_regex = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/;
+  var c = rgb.match(rgb_regex);
+  if (c) {
+    return "#" + toHex(c[1]) + toHex(c[2]) + toHex(c[3]);
+  }
+}
--- js/colorwheel.js
+++ js/colorwheel.js
@@ -5,31 +5,6 @@ $(function(){
   var check_single_digit = function(num) {
     return (num.toString().length === 1) ? "0" + num.toString() : num;
   }
-  var toRGB = function(num) { return parseInt(num,16); }
-  var toHex = function(num) { return check_single_digit(parseInt(num,10).toString(16)); }
-  var parseHex = function(hex) {
-    if (hex.charAt(0) === '#') hex = hex.substr(1);
-	var hex_regex = /^(?:[0-9a-fA-F]{3}){1,2}$/;
-	if (hex.match(hex_regex)) {
-	  var c = [];
-	  if (hex.length === 6) {
-	    c = hex.match(/.{1,2}/g);
-	  } else if (hex.length === 3) {
-	    c = [hex[0]+hex[0], hex[1]+hex[1], hex[2]+hex[2]];
-	  }
-	  for (var i = 0; i < c.length; i++) {
-	    c[i] = toRGB(c[i]);
-	  }
-	  return 'rgb(' + c.join(',') + ')';
-	}
-  }
-  var parseRGB = function(rgb) {
-    var rgb_regex = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/;
-    var c = rgb.match(rgb_regex);
-    if (c) {
-      return "#" + toHex(c[1]) + toHex(c[2]) + toHex(c[3]);
-    }
-  }
   var $hex = $('#hextext'), $rgb = $('#rgbtext'), $red = $('#red'),
 	$green = $('#green'), $blue = $('#blue'), $singlergb = $('#singlergb'),
 	$color = $('#color');
