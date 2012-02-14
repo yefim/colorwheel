b10804f06ce924cbc64b765916cd027a2094eae5
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-02-13 22:53:10 -0500

    added much better icon

--- js/colorwheel.js
+++ js/colorwheel.js
@@ -1,11 +1,31 @@
 $(function(){
-  var $hex = $('#hextext'), $rgb = $('#rgbtext'), $both = $('.colortext'), $color = $('#color');
+  var parseHex = function(hex) {
+	if (hex.charAt(0) === '#')
+		hex = hex.substr(1);
+	if (hex.length !== 6)
+		return hex;
+	var red = hex.substr(0,2);
+	var green = hex.substr(2,4);
+	var blue = hex.substr(4,6);
+	return parseInt(red, 10) + "," + parseInt(green, 10) + "," + parseInt(blue, 10);
+  }
+  var parseRGB = function(rgb) {
+	parseInt(hexString, 16);
+	return rgb;
+  }
+  var $hex = $('#hextext'), $rgb = $('#rgbtext'), $color = $('#color');
   $hex.val('#rrggbb');
   $rgb.val('rgb');
   $color.val('color');
   
-  $both.focus(function() {
-	$(this).val('');
+  $hex.focus(function() {
+	if ($hex.val() === '#rrggbb')
+	  $hex.val('');
+  });
+  
+  $rgb.focus(function() {
+	if ($rgb.val() === 'rgb')
+	  $rgb.val('');
   });
   
   $hex.blur(function() {
@@ -18,9 +38,9 @@ $(function(){
   });
   
   $hex.keyup(function() {
-	$rgb.val($hex.val());
+	$rgb.val(parseHex($hex.val()));
   });
   $rgb.keyup(function() {
-	$hex.val($rgb.val());
+	$hex.val(parseRGB($rgb.val()));
   });
 });
