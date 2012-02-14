0895af6bb6e1f527ece29b13f5cc866cc71ee774
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-02-14 02:34:25 -0500

    started a color class

---
+++ js/color.js
@@ -0,0 +1,9 @@
+function Color(red,green,blue) {
+  this.r = red;
+  this.g = green;
+  this.b = blue;
+}
+Color.prototype.toString() {
+  return 'rgb(' + r + "," + g "," + "c" + ')';
+}
+
--- js/colorwheel.js
+++ js/colorwheel.js
@@ -3,7 +3,7 @@ $(function(){
 	default_red = "R", default_green = "G", default_blue = "B";
 	
   var check_single_digit = function(num) {
-	return (num.toString().length === 1) ? "0" + num.toString() : num;
+    return (num.toString().length === 1) ? "0" + num.toString() : num;
   }
   var toRGB = function(num) { return parseInt(num,16); }
   var toHex = function(num) { return check_single_digit(parseInt(num,10).toString(16)); }
@@ -24,11 +24,11 @@ $(function(){
 	}
   }
   var parseRGB = function(rgb) {
-	var rgb_regex = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/;
-	var c = rgb.match(rgb_regex);
-	if (c) {
-		return "#" + toHex(c[1]) + toHex(c[2]) + toHex(c[3]);
-	}
+    var rgb_regex = /^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/;
+    var c = rgb.match(rgb_regex);
+    if (c) {
+      return "#" + toHex(c[1]) + toHex(c[2]) + toHex(c[3]);
+    }
   }
   var $hex = $('#hextext'), $rgb = $('#rgbtext'), $red = $('#red'),
 	$green = $('#green'), $blue = $('#blue'), $singlergb = $('#singlergb'),
@@ -37,11 +37,11 @@ $(function(){
   $green.val(default_green); $blue.val(default_blue); $color.html('color');
   
   $('body').keyup(function() {
-	if ($hex.val() !== "" && $rgb.val() !== "") {
-	  $color.css('background-color', $rgb.val());
-	} else {
-	  $color.css('background-color', 'fff');
-	}
+    if ($hex.val() !== "" && $rgb.val() !== "") {
+      $color.css('background-color', $rgb.val());
+    } else {
+      $color.css('background-color', 'fff');
+    }
   });
   $singlergb.map(function() {
     return $(this).val();
@@ -50,34 +50,34 @@ $(function(){
 	$hex.select();
   });
   $rgb.click(function() {
-	$rgb.select();
+    $rgb.select();
   });
   $hex.focus(function() {
-	if ($hex.val() === default_hex)
-	  $hex.val('');
+    if ($hex.val() === default_hex)
+      $hex.val('');
   });
   $rgb.focus(function() {
-	if ($rgb.val() === default_rgb)
-	  $rgb.val('');
+    if ($rgb.val() === default_rgb)
+      $rgb.val('');
   });
   $hex.blur(function() {
-	if ($hex.val() === '')
-	  $hex.val(default_hex);
+    if ($hex.val() === '')
+      $hex.val(default_hex);
   });
   $rgb.blur(function() {
-	if ($rgb.val() === '')
-	  $rgb.val(default_rgb);
+    if ($rgb.val() === '')
+      $rgb.val(default_rgb);
   });
   $hex.keyup(function() {
-	$rgb.val(parseHex($hex.val()));
+    $rgb.val(parseHex($hex.val()));
   });
   $rgb.keyup(function() {
-	$hex.val(parseRGB($rgb.val()));
+    $hex.val(parseRGB($rgb.val()));
   });
   // HELP
   $singlergb.children().keyup(function() {
     $singlergb.map(function() {
-	  return $(this).val();
+      return $(this).val();
     }).get().join(',');
   });
 });
