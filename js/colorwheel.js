3d1a53df28bc16c11c08d93d67a4d35212edb38c
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-02-13 23:11:06 -0500

    finished the conversions from hex to rgb and back

--- js/colorwheel.js
+++ js/colorwheel.js
@@ -5,18 +5,31 @@ $(function(){
 	if (hex.length !== 6)
 		return hex;
 	var red = hex.substr(0,2);
-	var green = hex.substr(2,4);
-	var blue = hex.substr(4,6);
-	return parseInt(red, 10) + "," + parseInt(green, 10) + "," + parseInt(blue, 10);
+	var green = hex.substr(2,2);
+	var blue = hex.substr(4,2);
+	return parseInt(red, 16) + "," + parseInt(green, 16) + "," + parseInt(blue, 16);
   }
   var parseRGB = function(rgb) {
-	parseInt(hexString, 16);
-	return rgb;
+	var color = rgb.split(',');
+	var hex = "#";
+	if (color.length !== 3)
+	  return;
+	for (var i = 0; i < color.length; i++) {
+	  hex = hex + parseInt(color[i],10).toString(16);
+	}
+	return hex;
   }
   var $hex = $('#hextext'), $rgb = $('#rgbtext'), $color = $('#color');
   $hex.val('#rrggbb');
   $rgb.val('rgb');
   $color.val('color');
+  
+  $hex.click(function() {
+	$hex.select();
+  });
+  $rgb.click(function() {
+	$rgb.select();
+  });
   
   $hex.focus(function() {
 	if ($hex.val() === '#rrggbb')
