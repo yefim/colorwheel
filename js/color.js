db5e96c126eadde840959fd8c34ed41e425bc6fb
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-02-16 03:33:23 -0500

    finished designing it and setting click events, everything works well

--- js/color.js
+++ js/color.js
@@ -2,8 +2,8 @@ var parseColor = function(c) {
   if (c.charAt(0) === '#') c = c.substr(1);
   // hex, rgb, single
   var hex_regex = /^([0-9a-fA-F]{2})([0-9a-fA-F]{2})([0-9a-fA-F]{2})$/;
-  var rgb_regex = /^rgb\s*\((\d+),\s*(\d+),\s*(\d+)\)$/i;
-  var single_regex = /^([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]),([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]),([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])$/;
+  var rgb_regex = /^rgb\s*\(([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]),\s*([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]),\s*([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])\)$/i;
+  var single_regex = /^([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]),\s*([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5]),\s*([01]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])$/;
   var color = [];
   color = c.match(hex_regex);
   if (color != null) {
@@ -26,7 +26,7 @@ function Color(r,g,b) {
 Color.prototype.convert = function(into) {
   // break statements are unnecessary because I'm always returning shit
   switch(into) {
-    case 'rgb': return 'rgb('+this.red+','+this.green+','+this.blue + ')';
+    case 'rgb': return 'rgb('+this.red+', '+this.green+', '+this.blue + ')';
     case 'hex': return '#'+toHex(this.red)+''+toHex(this.green)+''+ toHex(this.blue);
     case 'red': return this.red;
     case 'green': return this.green;
--- js/colorwheel.js
+++ js/colorwheel.js
@@ -1,20 +1,26 @@
 $(function(){	
+  var error_color = '#FFF8F8';
   var $color = $('#color'), $input = $('#input'), $boxes = $('.colortext');
+
+  $input.css('background-color', error_color);
   
   $boxes.each(function() {
     $this = $(this);
-    $this.html($this.attr('data-default'));
+    $this.text($this.attr('data-default'));
   });
   
   $input.keyup(function() {
     var color = parseColor($input.val());
     if (color) {
+      $input.css('background-color', '#fff');
       $boxes.each(function() {
         var $current = $(this);
         var into = $current.attr('id');
-        $current.html(color.convert(into));
+        $current.text(color.convert(into));
       });
       $color.css('background-color', color.convert('rgb'));
+    } else {
+      $input.css('background-color', error_color);
     }
   }).click(function() {
     $input.select();
@@ -22,8 +28,19 @@ $(function(){
   
   $boxes.click(function() {
     $this = $(this);
-    if ($this.html() !== $this.attr('data-default'))
-      $input.val($this.html());
+    if ($this.attr('class').indexOf('single') > -1) {
+      var rgb = [];
+      $('.single').each(function(i) {
+        rgb[i] = $(this).text();
+      });
+      $input.val(rgb.join(', '));
+    } else {
+      $input.val($this.text());
+    }
+    $input.select();
+  });
+  $color.click(function() {
+    $input.val($color.css('background-color'));
     $input.select();
   });
 });
