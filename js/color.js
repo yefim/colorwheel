45ddc86f40b8e7ef9c7a4bcb8f84cf1097466f61
Geoffrey Vedernikoff <yefim323@gmail.com>
2012-02-17 01:27:43 -0500

    made the HSV textboxes prettier

--- js/color.js
+++ js/color.js
@@ -19,14 +19,15 @@ var parseColor = function(c) {
   }
 }
 function Color(r,g,b) {
-  this.red = Math.round(r);
-  this.green = Math.round(g);
-  this.blue = Math.round(b);
+  this.red = r;
+  this.green = g;
+  this.blue = b;
+  this.rgb = r+", "+g+", "+b;
 }
 Color.prototype.convert = function(into) {
   // break statements are unnecessary because I'm always returning shit
   switch(into) {
-    case 'rgb': return 'rgb('+this.red+', '+this.green+', '+this.blue + ')';
+    case 'rgb': return 'rgb('+this.rgb + ')';
     case 'hex': return '#'+toHex(this.red)+''+toHex(this.green)+''+ toHex(this.blue);
     case 'red': return this.red;
     case 'green': return this.green;
@@ -48,31 +49,28 @@ Color.prototype.toHSV = function() {
   var g = parseInt(this.green,10)/255;
   var b = parseInt(this.blue,10)/255;
 
-  var H, S, V;
+  var h, s, v;
   var min = Math.min(r,g,b ), max = Math.max(r,g,b);
-  var delta = max - min; //need to cover the case if delta === 0
-  V = max;    // v
+  var delta = max-min;
+  v = max;    // v
   if(max != 0) 
-    S = delta / max;  // s 
+    s = delta/max;  // s 
   else { 
-    // r = g = b = 0  // s = 0, v is undefined 
-    S = 0; 
-    H = -1;
+    // r = g = b = 0  // it's black!
     return new HSV(0,0,0);
   }
-  if (delta === 0) {
-    H = 0;
-  }
+  if (delta === 0)
+    h = 0;
   else if(r === max)
-    H = (g-b)/delta;  // between yellow & magenta
+    h = (g-b)/delta;  // between yellow & magenta
   else if(g === max) 
-    H = 2+(b-r)/delta; // between cyan & yellow 
+    h = 2+(b-r)/delta; // between cyan & yellow 
   else 
-    H = 4+(r-g)/delta; // between magenta & cyan
-  H *= 60;    // degrees 
-  if(H < 0) 
-    H += 360;
-  return new HSV(H,S,V);
+    h = 4+(r-g)/delta; // between magenta & cyan
+  h *= 60;    // degrees 
+  if(h < 0) 
+    h += 360;
+  return new HSV(h,s,v);
 }
 function HSV(h,s,v) {
   this.hue = h;
@@ -83,16 +81,16 @@ HSV.prototype.toRGB = function() {
   var i = 0;
   var f, p, q, t;
   var h = this.hue, s = this.saturation, v = this.value;
-  if(s == 0) {
+  if(s === 0) {
     // achromatic (grey)
     return new Color(v*255, v*255, v*255);
   }
   h /= 60;			// sector 0 to 5
   i = Math.floor(h);
-  f = h - i;			// factorial part of h
-  p = v * (1 - s);
-  q = v * (1 - s * f);
-  t = v * (1 - s * (1 - f));
+  f = h-i;			// factorial part of h
+  p = v*(1-s);
+  q = v*(1-s*f);
+  t = v*(1-s*(1-f));
   switch(i) {
     case 0: return new Color(v*255, t*255, p*255);
     case 1: return new Color(q*255, v*255, p*255);
--- js/colorwheel.js
+++ js/colorwheel.js
@@ -1,15 +1,18 @@
 $(function(){
   var $color = $('#color'), $input = $('#input'), $boxes = $('.colortext'),
-    $HSV = $('.HSV');
+    $HSV = $('.hsv');
 
   $input.select();
   
   $boxes.each(function() {
-    $this = $(this);
-    $this.text($this.attr('data-default'));
+    $(this).text($(this).attr('data-default'));
   });
   
   $input.keyup(function() {
+    if ($input.val().length === 0) {
+      $input.removeClass('error');
+      return;
+    }
     var color = parseColor($input.val());
     if (color) {
       var HSV = color.toHSV();
@@ -35,15 +38,12 @@ $(function(){
       $('.single').each(function(i) {
         rgb[i] = $(this).text();
       });
-      $input.val(rgb.join(', '));
+      $input.val(rgb.join(', ')).select();
     } else {
-      $input.val($this.text());
+      $input.val($this.text()).select();
     }
-    $input.select();
   });
   $color.click(function() {
-    $input.val($color.css('background-color'));
-    $input.trigger('keyup');
-    $input.select();
+    $input.val($color.css('background-color')).trigger('keyup').select();
   });
 });
